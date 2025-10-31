b37e6b5d87093a55466d7e3fc3366f698034455e
Viacheslav Klimkov <vklimkov@nvidia.com>
2025-10-31 12:31:39 +0100

    [HOTFIX]: make sure API still works if custom inputs are not provided
    Signed-off-by: Viacheslav Klimkov <vklimkov@nvidia.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -1033,6 +1033,7 @@ class Scheduler(SchedulerInterface):
                     req,
                     req_to_new_blocks[req.request_id].get_block_ids(),
                     num_scheduled_tokens[req.request_id],
+                    self.await_inputs,
                     prefill_token_ids=req._all_token_ids,
                 )
                 for req in scheduled_new_reqs
@@ -1043,6 +1044,7 @@ class Scheduler(SchedulerInterface):
                     req,
                     req_to_new_blocks[req.request_id].get_block_ids(),
                     num_scheduled_tokens[req.request_id],
+                    self.await_inputs,
                 )
                 for req in scheduled_new_reqs
             ]
@@ -1288,10 +1290,11 @@ class Scheduler(SchedulerInterface):
             # TODO: in `schedule` add a check that custom inputs are set
             # for resumed_reqs
             # Read only the scheduled portion of custom_inputs (for chunked prefill)
-            scheduled_tokens = num_scheduled_tokens[req_id]
-            new_custom_inputs.append(
-                req.read_custom_inputs(scheduled_tokens)
-            )
+            if self.await_inputs:
+                scheduled_tokens = num_scheduled_tokens[req_id]
+                new_custom_inputs.append(
+                    req.read_custom_inputs(scheduled_tokens)
+                )
 
         return CachedRequestData(
             req_ids=req_ids,
