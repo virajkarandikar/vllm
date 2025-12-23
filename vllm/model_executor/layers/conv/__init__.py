324a100cb0145c6578b04b03199d0325113fb42c
Viacheslav Klimkov <vklimkov@nvidia.com>
2025-12-23 13:58:47 +0100

    Toy conv - a model with a conv2d layer. Proto for fastconformer preprocessor.
    Signed-off-by: Viacheslav Klimkov <vklimkov@nvidia.com>

--- vllm/model_executor/layers/conv/__init__.py
+++ vllm/model_executor/layers/conv/__init__.py
@@ -2,12 +2,10 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 from vllm.model_executor.layers.conv.causal_conv2d_k3s2 import (
-    causal_conv2d_k3s2_fn,
-    causal_conv2d_k3s2_update,
+    depthwise_strided_conv2d_cached,
 )
 
 __all__ = [
-    "causal_conv2d_k3s2_fn",
-    "causal_conv2d_k3s2_update",
+    "depthwise_strided_conv2d_cached",
 ]
 
