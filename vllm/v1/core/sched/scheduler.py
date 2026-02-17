e86e7f2c7fce053694c09eea79eb3c073cea4b1b
Viacheslav Klimkov <vklimkov@nvidia.com>
2026-02-17 13:51:25 +0100

    scheduler: fix preempting requests when those await inputs
    Signed-off-by: Viacheslav Klimkov <vklimkov@nvidia.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -1056,6 +1056,9 @@ class Scheduler(SchedulerInterface):
         self.encoder_cache_manager.free(request)
         request.status = RequestStatus.PREEMPTED
         request.num_computed_tokens = 0
+        if self.await_inputs and request.custom_inputs is not None:
+            request.custom_inputs_num_consumed = 0
+            request.custom_inputs_ready = True
         if request.spec_token_ids:
             request.spec_token_ids = []
         request.num_preemptions += 1
